f113cb08b36f60c7faaf0a32a6ae5edaf1310ee3
Mark Christian Albinto <markchristianalbinto@yahoo.com>
2020-04-01 12:49:57 +0000

    commit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 ReactDOM.render(
-  <h1>Hello World<h1>,
+  <h1>Hello World</h1>,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <h1>Good Bye</h1>,
+  document.getElementById('others')
+
+
+);
+
+function MyName(props){
+    return <h1>The name {props.name} contains {props.number} characters!</h1>;
+}
+const name1 = <MyName name="Erika" number="5" />
+const name2 = <MyName name="George" number="6" />
+const name3 = <MyName name="Phillip" number="7" />
+
+ReactDOM.render(
+  name1,
+  document.getElementById('name1')
+);
+ReactDOM.render(
+  name2,
+  document.getElementById('name2')
+);
+ReactDOM.render(
+  name3,
+  document.getElementById('name3')
 );
